284eebb498a70ebbff7679f02d9f2590a0a93d8e
Andre Walker-Upshur <Andre Walker-Upshur>
2016-08-18 14:37:46 -0400

    Test search component

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var TodoSearch = React.createClass({
   handleSearch: function () {
     var showCompleted = this.refs.showCompleted.checked;
-    var searchText = tis.refs.searchText.value;
+    var searchText = this.refs.searchText.value;
 
     this.props.onSearch(showCompleted, searchText);
   },
