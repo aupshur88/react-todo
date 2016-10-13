4f253e8dc843a9677ac97a65f191233af98cca5d
Andre Walker-Upshur <Andre Walker-Upshur>
2016-10-13 14:58:31 -0400

    move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
   var config = {
-      apiKey: "AIzaSyCaKvOgcX17nkcSgW-Iynp5bfmtLF8q4zQ",
-      authDomain: "upshur-todo-app.firebaseapp.com",
-      databaseURL: "https://upshur-todo-app.firebaseio.com",
-      storageBucket: "upshur-todo-app.appspot.com",
-      messagingSenderId: "25623470448"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
 
     firebase.initializeApp(config);
